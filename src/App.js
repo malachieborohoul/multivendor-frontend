f0e48551ccbbb476429ba0282e87031678a7eb0a
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-19 22:08:54 +0100

    Front end with React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Python Market Place
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link " aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Categories
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <main className="mt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-3 mb-2">
+              <div className="card">
+                <img src={logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Product title</h5>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
