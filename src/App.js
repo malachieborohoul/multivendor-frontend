0ff6db4dea87cf563f2e883b786deb88d1021a36
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-21 23:46:22 +0100

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-// functionfunctionfunctionfunctionfunctionfunc
+// functionfunctionfunctionfunctionfunctionfun
 function App() {
   return (
     <>
