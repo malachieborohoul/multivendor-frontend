84ee7e1eca45148c07ebf3fe2d706facd8443e67
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-24 23:57:09 +0100

    Rating & Reviews

--- src/App.js
+++ src/App.js
@@ -83,28 +83,71 @@ function App() {
           </div>
 
           {/* Rating and Reviews */}
-          <div id="carouselExample" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="..."/>
+          <div id="carouselExample" className="carousel slide bg-dark text-white p-5">
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>A well-known quote, contained in a blockquote element.</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                 <i className="fa fa-star text-warning"></i> 
+                 <i className="fa fa-star text-warning"></i> 
+                 <i className="fa fa-star text-warning"></i> 
+                 <i className="fa fa-star text-warning"></i> 
+                 <cite title="Source Title">Customer Name</cite>
+                </figcaption>
+              </figure>
               </div>
-              <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..."/>
+              <div className="carousel-item">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>A well-known quote, contained in a blockquote element.</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </figcaption>
+              </figure>
               </div>
-              <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="..."/>
+              <div className="carousel-item">
+              <figure className="text-center">
+                <blockquote className="blockquote">
+                  <p>A well-known quote, contained in a blockquote element.</p>
+                </blockquote>
+                <figcaption className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </figcaption>
+              </figure>
               </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <span className="visually-hidden">Next</span>
             </button>
           </div>
           {/* Rating and Reviews End */}
+          {/*  RatingRatingRatingRatin*/}
+          <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mt-5">
+            <div className="col-md-4 d-flex align-items-center">
+              <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                {/* <svg className="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg> */}
+              </a>
+              <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+            </div>
+
+            <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+
+              <li className="ms-3"><a className="text-body-secondary" href="#"><i className="fa-brands fa-facebook fa-2x"></i></a></li>
+              <li className="ms-3"><a className="text-body-secondary" href="#"><i className="fa-brands fa-instagram fa-2x"></i></a></li>
+              <li className="ms-3"><a className="text-body-secondary" href="#"><i className="fa-brands fa-twitter fa-2x"></i></a></li>
+              {/* <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li> */}
+              {/* <li className="ms-3"><a className="text-body-secondary" href="#"><svg className="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li> */}
+            </ul>
+          </footer>
 
 
 
