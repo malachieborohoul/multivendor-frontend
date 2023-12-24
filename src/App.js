06b14c6cd072608740d4edf6bf31b45a22361724
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-24 22:14:07 +0100

    Latest Products Box

--- src/App.js
+++ src/App.js
@@ -88,7 +88,32 @@ function App() {
           {/* Category Box End*/}
           </div>
 
+
+          <h3 className="mb-4">Popular Sellers <a  className="float-end btn btn-sm btn-dark mt-0 " href="#">View all Sellers <i className="fa fa-arrow-right-long mt-2" aria-hidden="true"></i></a></h3>
+          <div className="row">
+          {/* Category Box */}
+
+            <div className="col-12 col-md-3 mb-3">
+              <div className="card shadow">
+                <img src={logo} className="card-img-top" alt="..." />
+                <hr/>
+                <div className="card-body">
+                  <h4 className="card-title">Category title</h4>
+                  <h5 className="card-title text-muted">Price Rs. 500</h5>
+                  <div className="card-footer">
+                    
+                    Categories: <a href="#">Python</a>,<a href="#">PHP</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          {/* Category Box End*/}
+          </div>
+
         </div>
+
+
+        
       </main>
     </>
   );
