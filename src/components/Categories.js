a77e63bb65d2591e536f5e7077d5dfdfb9104a66
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-25 01:46:27 +0100

    Category List Router

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,29 @@
+import logo from "../logo.svg";
+
 function Categories(params) {
     return (
-        <h1>Category List</h1>
+    <section className="container">
+        <h3 className="mb-4">Popular Categories <a  className="float-end btn btn-sm btn-dark mt-0 " href="#">View all Categories <i className="fa fa-arrow-right-long mt-2" aria-hidden="true"></i></a></h3>
+      <div className="row">
+      {/* Category Box */}
+
+        <div className="col-12 col-md-3 mb-3">
+          <div className="card shadow">
+            <img src={logo} className="card-img-top" alt="..." />
+            <hr/>
+            <div className="card-body">
+              <h4 className="card-title">Category title</h4>
+              <h5 className="card-title text-muted">Price Rs. 500</h5>
+              <div className="card-footer">
+                
+                Product Downloads: 2344 
+              </div>
+            </div>
+          </div>
+        </div>
+      {/* Category Box End*/}
+      </div>
+    </section>
     )
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import logo from "../logo.svg";
+import Categories from "./Categories";
 
 function Home(){
     return(
@@ -33,27 +34,7 @@ function Home(){
 
 
 
-    <h3 className="mb-4">Popular Categories <a  className="float-end btn btn-sm btn-dark mt-0 " href="#">View all Categories <i className="fa fa-arrow-right-long mt-2" aria-hidden="true"></i></a></h3>
-          <div className="row">
-          {/* Category Box */}
-
-            <div className="col-12 col-md-3 mb-3">
-              <div className="card shadow">
-                <img src={logo} className="card-img-top" alt="..." />
-                <hr/>
-                <div className="card-body">
-                  <h4 className="card-title">Category title</h4>
-                  <h5 className="card-title text-muted">Price Rs. 500</h5>
-                  <div className="card-footer">
-                    
-                    Product Downloads: 2344 
-                  </div>
-                </div>
-              </div>
-            </div>
-          {/* Category Box End*/}
-          </div>
-
+        <Categories/>
 
           <h3 className="mb-4">Popular Sellers <a  className="float-end btn btn-sm btn-dark mt-0 " href="#">View all Sellers <i className="fa fa-arrow-right-long mt-2" aria-hidden="true"></i></a></h3>
           <div className="row">
