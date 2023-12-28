4d0eae2fc93e6679486c3bc3bf67a5af773967f5
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-28 11:35:30 +0100

    CategoryProducts

--- src/components/CategoryProducts.js
+++ src/components/CategoryProducts.js
@@ -1,21 +1,56 @@
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import SingleProduct from "./SingleProduct";
+import { useEffect, useState } from "react";
 
 function CategoryProducts(params) {
+//   let products=[
+//     {
+//       "title":"Python",
+//       "price":100
+//     },
+//     {
+//       "title":"Django",
+//       "price":120
+//     }
+// ]
+const baseUrl = 'http://localhost:8000/api';
+
+const [products, setProducts]=useState([])
+const [totalResults, setTotalResults]=useState(0)
+
+useEffect(()=>{
+  fetchData(baseUrl+'/products/');
+},[])
+
+function fetchData(baseurl){
+  fetch(baseurl)
+  .then((response)=>response.json())
+  .then((data)=>{
+    setProducts(data.results)
+    setTotalResults(data.count)
+  })
+}
+
+function changeUrl (baseurl) { 
+  fetchData(baseurl)
+ }
+const links= [];
+for (let i =0 ;i<totalResults; i++){
+  links.push( <li class="page-item"><Link onClick={()=>{changeUrl(baseUrl+`/products/?page=${i+1}`)}} to={`/products/?page=${i+1}`} class="page-link" href="#">{i+1}</Link></li>)
+}
+
+
     return (
     <section className="container mt-4">
         <h3 className="mb-4"><span className="text-success">Python</span> Products</h3>
       <div className="row">
             {/* Product Box */}
-
-           <SingleProduct title="Django"/>
-           <SingleProduct/>
-           <SingleProduct/>
-           <SingleProduct/>
-           <SingleProduct/>
-           <SingleProduct/>
-           <SingleProduct/> 
+          {products.map((product)=>{
+            return <SingleProduct product={product}/>
+          })}
+           
+          
           {/* Product Box End*/} 
       </div>
 
@@ -26,9 +61,8 @@ function CategoryProducts(params) {
               <span aria-hidden="true">&laquo;</span>
             </a>
           </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          {links}
+
           <li class="page-item">
             <a class="page-link" href="#" aria-label="Next">
               <span aria-hidden="true">&raquo;</span>
