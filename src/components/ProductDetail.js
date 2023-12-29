7cf95fe518d50a3de03d56f5e1bc630633963d72
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-29 16:19:29 +0100

    Product Detail

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -3,7 +3,7 @@ import logo from "../logo.svg";
 import SingleProduct from "./SingleProduct";
 import { useEffect, useState } from "react";
 import axios from 'axios'
-//  Produ
+//  Prod
 function ProductDetail() {
   const baseUrl = 'http://127.0.0.1:8000/api';
 
