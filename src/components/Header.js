b4f852b23ae932113b63c57e57471f1b2a9f05d6
malachieborohoul <malachieborohoul@gmail.com>
2023-12-30 23:56:34 +0100

    userContext

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { UserContext } from '../Context'
 import { useContext,  } from 'react'
 
 function Header() { 
-  // userContextuserConte
+  // userContextuserCont
   const userContext=useContext(UserContext)
 
   console.log(userContext)
