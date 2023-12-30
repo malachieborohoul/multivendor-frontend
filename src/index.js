c117fbdb425f30e92a706f6b2e94f4eefd9a51ce
malachieborohoul <malachieborohoul@gmail.com>
2023-12-30 23:17:10 +0100

    Context

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserContext } from './Context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <UserContext.Provider value="1"> 
+        <App />
+      </UserContext.Provider>
+     
     </Router>
    
   </React.StrictMode>
