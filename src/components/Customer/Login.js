91beb45e651c0a7b3b0b78a694e3c0ce85901157
Borohoul Soguelni Malachie <malachieborohoul@gmail.com>
2023-12-30 01:04:20 +0100

    Login

--- src/components/Customer/Login.js
+++ src/components/Customer/Login.js
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
 import logo from "../../logo.svg";
+import { useState } from "react";
+
+
+
 
 function Login(props) {
+  const[loginFormData, setLoginFormData]=useState({
+    'username':'',
+    'password':''
+  }) 
+
+ const inputHandler=(event)=>{
+    setLoginFormData({
+      ...loginFormData,[event.target.name]:event.target.value
+    })
+  }
+
+  console.log(loginFormData)
     return (
    
 
@@ -10,12 +26,12 @@ function Login(props) {
               <form>
 
                 <div className="mb-3">
-                  <label for="username" name="username"  className="form-label">Username</label>
-                  <input type="email" className="form-control" id="username" aria-describedby="emailHelp"/>
+                  <label for="username"  className="form-label">Username</label>
+                  <input type="text"  name="username" className="form-control" value={loginFormData.username} onChange={inputHandler} id="username" aria-describedby="emailHelp"/>
                 </div>
                 <div className="mb-3">
                   <label for="password" className="form-label">Password</label>
-                  <input type="password" className="form-control" id="password"/>
+                  <input type="password" className="form-control" name="password" id="password" value={loginFormData.password} onChange={inputHandler}/>
                 </div>
                
                 <button type="submit" className="btn btn-primary">Submit</button>
