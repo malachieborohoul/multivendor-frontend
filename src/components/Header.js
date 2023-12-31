a699eba9280891f44f4a04cf3c7fa325ba71603b
malachieborohoul <malachieborohoul@gmail.com>
2023-12-31 13:47:55 +0100

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { CartContext, UserContext } from '../Context'
 import { useContext,  } from 'react'
 
 function Header() { 
-  // HeaderHeaderHeaderHeader
+  // HeaderHeaderHeaderHeade
   const userContext=useContext(UserContext)
   const {cartData, setCartData}= useContext(CartContext)
 
